ad63feb97425c275765641530a46bf99e8fff522
mhamad raad <hamaraad883@gmail.com>
2023-10-29 18:08:51 +0300

    basic structure of the app

--- App.js
+++ App.js
@@ -1,34 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.playText}>Hello World!</Text>
-      <TextInput placeholder='Your Course Goal' />
-      <Button
-        title='Tap Me!'
-        color='#841584'
-        onPress={() => {}}
-        accessibilityLabel='Learn more about this purple button'
-      />
-      <StatusBar style='auto' />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your Course Goal' />
+        <Button
+          title='Add Goal'
+          color='#841584'
+          onPress={() => {}}
+          accessibilityLabel='Learn more about this purple button'
+        />
+      </View>
+      <View style={styles.goalsConatiner}>
+        <Text>List of goals!</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  playText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: 'red',
-    padding: 16,
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsConatiner: {
+    flex: 5,
   },
 });
 
